323f01ce0f3f252672f1177a30e2e9499abcb68a
Marco don Kaulea <DonMarco42@gmail.com>
2014-12-12 17:03:41 +0100

    Add key generation and decryption

--- keepass.py
+++ keepass.py
@@ -8,37 +8,106 @@
 # ----------------------------------------------------------------------------
 
 
+import Crypto.Cipher.AES as AES
 import struct
+import hashlib
+
+
+def parse_comment(content):
+    return dict(comment=content)
+
+
+def parse_cypher_id(content):
+    return dict(cypher_id=content)
+
+
+def parse_compression_flags(content):
+    return dict(compression_flags=struct.unpack('I', content)[0])
+
+
+def parse_master_seed(content):
+    return dict(master_seed=content)
+
+
+def parse_transform_seed(content):
+    return dict(transform_seed=content)
+
+
+def parse_transform_rounds(content):
+    return dict(transform_rounds=struct.unpack('L', content)[0])
+
+
+def parse_encryption_iv(content):
+    return dict(encryption_iv=content)
+
+
+def parse_protected_stream_key(content):
+    return dict(protected_stream_key=content)
+
+
+def parse_stream_start_bytes(content):
+    return dict(stream_start_bytes=content)
+
+
+def parse_inner_random_stream_id(content):
+    return dict(inner_random_stream_id=content)
+
+
+header_parser = {0x01: parse_comment,
+                 0x02: parse_cypher_id,
+                 0x03: parse_compression_flags,
+                 0x04: parse_master_seed,
+                 0x05: parse_transform_seed,
+                 0x06: parse_transform_rounds,
+                 0x07: parse_encryption_iv,
+                 0x08: parse_protected_stream_key,
+                 0x09: parse_stream_start_bytes,
+                 0x0a: parse_inner_random_stream_id}
 
 
 def handle_kdbx(kdbx_file):
-    print("Keepass2.x")
     unknown = kdbx_file.read(4)
-    print("Not idea what this is. {}", unknown)
+    header = parse_header(kdbx_file)
+    key = generate_final_master_key(generate_composite_key('asdfg'),
+                                    header['transform_seed'],
+                                    header['transform_rounds'],
+                                    header['master_seed'])
+    aes = AES.new(key,
+                  mode=AES.MODE_CBC,
+                  IV=header['encryption_iv'])
+    body = kdbx_file.read() + b'\00\00\00\00\00\00\00\00\00\00\00\00'
+    body = aes.decrypt(body)
+    print(body)
+
+
+def parse_header(kdbx_file):
+    header = dict()
     type, length = struct.unpack('=BH', kdbx_file.read(3))
     while 0x00 != type:
         content = kdbx_file.read(length)
-        if 0x01 == type:
-            print("Comment: {}", content)
-        elif 0x02 == type:
-            print("Cypher ID: {}", content)
-        elif 0x03 == type:
-            print("Compression Flags: {}", content)
-        elif 0x04 == type:
-            print("Master Seed: {}", content)
-        elif 0x05 == type:
-            print("Transform Seed: {}", content)
-        elif 0x06 == type:
-            print("Transform Rounds: {}", content)
-        elif 0x07 == type:
-            print("Encryption IV: {}", content)
-        elif 0x08 == type:
-            print("Protected Stream Key: {}", content)
-        elif 0x09 == type:
-            print("Stream Start Bytes: {}", content)
-        elif 0x0a == type:
-            print("Inner Random Stream ID: {}", content)
+        header.update(header_parser[type](content).items())
         type, length = struct.unpack('=BH', kdbx_file.read(3))
+    return header
+
+
+def generate_composite_key(password, keyfile=None):
+    hash = hashlib.sha256(password.encode('utf-8')).digest()
+    if keyfile is not None:
+        hash += hashlib.sha256(keyfile).digest()
+    return hashlib.sha256(hash).digest()
+
+
+def generate_final_master_key(composite_key,
+                              transform_seed,
+                              transform_rounds,
+                              master_seed):
+    aes = AES.new(transform_seed)
+    transformed_key = composite_key
+    for i in range(transform_rounds):
+        transformed_key = aes.encrypt(transformed_key)
+    transformed_key = hashlib.sha256(transformed_key).digest()
+    final_master_key = hashlib.sha256(master_seed + transformed_key).digest()
+    return final_master_key
 
 
 def main():
